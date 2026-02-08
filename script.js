5668853b674094a0d5ea0763f3029d850f173070
unknown <ign.frei@gmail.com>
2026-02-08 17:29:09 -0300

    updates

--- script.js
+++ script.js
@@ -112,4 +112,32 @@ const observer2 = new IntersectionObserver(
 animatedElements.forEach(el => observer.observe(el));
 
 
+// Modal para dresscode //
+
+// Obtener el modal
+var modal = document.getElementById("dressCodeModal");
+
+// Obtener el botón que abre el modal
+var btn = document.getElementById("dressCodeBtn");
+
+// Obtener el elemento <span> que cierra el modal
+var span = document.getElementsByClassName("close")[0];
+
+// Cuando el usuario hace clic en el botón, abre el modal 
+btn.onclick = function() {
+    modal.style.display = "block";
+}
+
+// Cuando el usuario hace clic en <span> (x), cierra el modal
+span.onclick = function() {
+    modal.style.display = "none";
+}
+
+// Cuando el usuario hace clic en cualquier parte fuera del modal, también lo cierra
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
+
 
